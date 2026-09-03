3b7fbf27a1316d519277be3a08b776acad6f5469
nrolabb <sharesnro@gmail.com>
2026-09-03 18:04:37 +0700

    fix lỗi xóa nhân vật

--- src/nro/models/player/EffectSkill.java
+++ src/nro/models/player/EffectSkill.java
@@ -244,7 +244,6 @@ public class EffectSkill {
         }
         if (isBienHinh && wasSpineBienHinh != nro.models.services.EffectSkillService.gI().isUseSpineBienHinh(player)) {
             wasSpineBienHinh = !wasSpineBienHinh;
-            Service.gI().player(player);
             Service.gI().Send_Caitrang(player);
             int iconLvFirst = (player.gender == nro.models.consts.ConstPlayer.TRAI_DAT) ? 31254 : (player.gender == nro.models.consts.ConstPlayer.NAMEC) ? 31266 : (player.gender == nro.models.consts.ConstPlayer.XAYDA) ? 31260 : 0;
             int timeIcon = (int) ((lastTimeBienHinh + timeBienHinh - System.currentTimeMillis()) / 1000);
